ee795704371d67aa1b1e945a6c6a200f825372c8
Enock <enocksm@outlook.com>
2024-04-09 19:58:02 -0300

    Adicionando mais tratamento de exceções.

--- src/main/java/com/example/demo/repos/PeopleRepository.java
+++ src/main/java/com/example/demo/repos/PeopleRepository.java
@@ -1,7 +1,6 @@
 package com.example.demo.repos;
 
 import com.example.demo.domain.people.People;
-import com.example.demo.domain.school.School;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
